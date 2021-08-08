377b14fc9e250bfdd662a94093d0db0398157319
Mario Gastegger <mario.gastegger@gmail.com>
2021-08-08 13:57:30 +0200

    Add link to master Just 4 Fun implementation

--- src/examples.jl
+++ src/examples.jl
@@ -14,16 +14,22 @@ module Examples
   include("../games/mancala/main.jl")
   export Mancala
 
+  include("../games/just4fun/main.jl")
+  export Just4Fun
+
+
   const games = Dict(
     "grid-world" => GridWorld.GameSpec(),
     "tictactoe" => Tictactoe.GameSpec(),
     "connect-four" => ConnectFour.GameSpec(),
-    "mancala" => Mancala.GameSpec())
+    "mancala" => Mancala.GameSpec(),
+    "just4fun" => Just4Fun.GameSpec())
 
   const experiments = Dict(
     "grid-world" => GridWorld.Training.experiment,
     "tictactoe" => Tictactoe.Training.experiment,
     "connect-four" => ConnectFour.Training.experiment,
-    "mancala" => Mancala.Training.experiment)
+    "mancala" => Mancala.Training.experiment,
+    "just4fun" => Just4Fun.Training.experiment)
 
 end
